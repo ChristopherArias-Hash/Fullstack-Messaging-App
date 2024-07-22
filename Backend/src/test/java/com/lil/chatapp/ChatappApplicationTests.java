66834efe1c51dd187b916d4660439bc3edff0bd9
ChristopherArias-Hash <christopher.arias855@myci.csuci.edu>
2024-07-21 19:06:28 -0700

    Adding frontend and backend files

---
+++ Backend/src/test/java/com/lil/chatapp/ChatappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lil.chatapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
